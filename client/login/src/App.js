176874fc12f96bd8c5cb4e1aeaaa1c21d65b129f
kaveenakash <kaveenakashya@gmail.com>
2021-02-04 08:49:27 +0530

    Base Setup done

---
+++ client/login/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName" />
+        <label>Review:</label>
+        <input type="text" name="review" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
